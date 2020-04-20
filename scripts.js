d0ef0ef5a1ba7318edb8ef6bf45439c3e7fa88fd
Kaio Dolfini <kaio.dolfini@gmail.com>
2020-04-20 12:09:14 -0300

    Adicionando Desafio Final Completo

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
+const modal = document.querySelector(".modal");
 
 for(let card of cards){
     card.addEventListener("click", function(){
@@ -12,4 +13,14 @@ for(let card of cards){
 closeModal = document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove("active");
     modalOverlay.querySelector("iframe").src = "";
+})
+
+maximizeModal = document.querySelector(".maximize-modal").addEventListener("click", function(){
+
+    modal.classList.toggle("maximize")
+    // if (modalOverlay.classList.contains("maximize")){
+    //     modalOverlay.classList.remove("maximize");
+    // } else{
+    //     modalOverlay.classList.add("maximize");
+    // }
 })
